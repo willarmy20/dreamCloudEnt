dfe8382a5f600a4d16588465f6622eae03d8bf59
willarmy20 <81721198+willarmy20@users.noreply.github.com>
2021-08-24 19:43:07 -0400

    updated componets

--- App.js
+++ App.js
@@ -1,59 +1,21 @@
-import 'react-native-gesture-handler';
-import React from "react";
-//Navigation componets
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Ionicons from 'react-native-vector-icons/Ionicons'
-import { Text, View } from 'react-native';
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import Navigation from './src/components/Navigation';
 
 
-function HomeScreen(){
-    return(
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>DreamCloudEnt</Text>
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Navigation />
     </View>
   );
-}
-function AboutScreen(){
-    return(
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>About!</Text>
-    </View>
-  );
-}
-function ThemeScreen(){
-    return(
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Theme!</Text>
-    </View>
-  );
-}
+};
 
-const Tab = createBottomTabNavigator();
-export default function App(){
-  return(
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({route}) => ({
-          tabBarIcon: ({focused, color, size, padding}) =>{
-              let iconName;
-              if(route.name === "DreamCloudEnt"){
-                  iconName = focused ? "home" : "home-outline";
-              } else if (route.name === "About"){
-                  iconName = focused ? "information-circle" : "information-circle-outline";
-              }else if(route.name === "Theme"){
-                  iconName = focused ? "apps" : "apps-outline";
-              }
-              return <Ionicons name={iconName} size={size} color={color} />;
-            },
-            tabBarActiveTintColor: 'lightseagreen',
-            tabBarInactiveTintColor: 'gray',
-        })}
-      >
-        <Tab.Screen name={"DreamCloudEnt"} component={HomeScreen}></Tab.Screen>
-        <Tab.Screen name={"About"} component={AboutScreen}></Tab.Screen> 
-        <Tab.Screen name={"Theme"} component={ThemeScreen}></Tab.Screen>
-      </Tab.Navigator>
-    </NavigationContainer>
-);
-}
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    paddingTop: 60
+  },
+});
+
+export default App;
